d3ae9a644b7de340aa85e6a5234fe8d5eb7c3923
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2024-09-24 04:59:23 +0600

    blog FAQ section design added

--- js/blog-target.js
+++
@@ -1,6 +0,0 @@
-// document.getElementById('blog-donation').addEventListener('click', function(e){
-//     e.preventDefault();
-//     document.getElementById('blog-main').classList.add('hidden');
-//     document.getElementById('blog-donation').classList.add('bg-green-500')
-//     document.getElementById('Donation-section').classList.remove('hidden');
-// } )
